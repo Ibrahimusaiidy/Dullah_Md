b5b70a0708214a87143a9788ef868ec0ce3a2a1b
Ibrahimusaiidy <saiidibrahim192@gmail.com>
2024-12-03 17:19:54 +0300

    Update deploy.js

--- commandes/deploy.js
+++ commandes/deploy.js
@@ -38,29 +38,29 @@ Hello ${nomAuteurMessage},,
 *DEPLOYMENT STEPS* 
 ╭───────────────────☆
 ★When you want to deploy any whatsapp bot check on its repo and check on its deployment procedure and make sure you have the deployment site e.g;  heroku.com , render.com , Koyeb.com and many more:
-✔First type the sc ,repo or script command and you will get dullah md repository 
+✔First type the sc ,repo or script command and you will get Mr b² md repository 
 ✔From there you are required to get your *Session id* but how,??..
          𝐇𝐎𝐖 𝐓𝐎 𝐆𝐄𝐓 𝐒𝐄𝐒𝐒𝐈𝐎𝐍 𝐈𝐃
-✞ Open this link 👉 https://dullah-sessions-pi5z.onrender.com then tap on pair code.
-✞ Enter your whatsapp number with the country code e.g;  255716945971 then tap submit 
-✞Dullah Md owner , *dullahmd* ,will send you the code immediately.Copy the code and whatsapp will bring a notification 
-✞Tap on the notification and paste the code that Dullah md owner sent you.
-✞ After a successful login Dullah Md owner 🤔 dullahmd will send you a *session id* .some will wonder where the hell is that session ,,,...It is just right at your inbox /dm {your own number for whatsappp} 😂
+✞ Open this link 👉 https://Mr b²-sessions-pi5z.onrender.com then tap on pair code.
+✞ Enter your whatsapp number with the country code e.g;  255628848298 then tap submit 
+✞Mr b² Md owner , *Mr b²md* ,will send you the code immediately.Copy the code and whatsapp will bring a notification 
+✞Tap on the notification and paste the code that Mr b² md owner sent you.
+✞ After a successful login Mr b² Md owner 🤔 Mr b²md will send you a *session id* .some will wonder where the hell is that session ,,,...It is just right at your inbox /dm {your own number for whatsappp} 😂
 ✞Copy the Session id {the one with unreadable codes and long copy it and send it to your deployer or deploy
-     𝐇𝐎𝐖 𝐓𝐎 𝐃𝐄𝐏𝐋𝐎𝐘 DULLAH 𝐌𝐃
-✔ Now check on Dullah md repository in github fork and give a star to this repository before doing anything 🌟or else dullah Md owner won't allow you to deploy his bot🤖.
+     𝐇𝐎𝐖 𝐓𝐎 𝐃𝐄𝐏𝐋𝐎𝐘 Mr b² 𝐌𝐃
+✔ Now check on Mr b² md repository in github fork and give a star to this repository before doing anything 🌟or else Mr b² Md owner won't allow you to deploy his bot🤖.
 ✔Tap on heroku deploy tab  given there. First thing you should do is getting your *Heroku Api Key* insert it to the required space .
 ✔Enter the valid *heroku app name* and again repeat it to the blank space asking for app name accordingly.Fill everything and press on the below tab Deploy
 ✔In some heroku apps the buld logs might not show but it will eventually deploy 
-✔Now click on this devs number and give dullah md owner credits https://wa.me/qr/255716945971
+✔Now click on this devs number and give Mr b² md owner credits https://wa.me/qr/255628848298
 ╰────────────────────☆`;
 let menuMsg = `
-     𝐑𝐞𝐠𝐚𝐫𝐝𝐬 DULLAH MD BOT`;
+     𝐑𝐞𝐠𝐚𝐫𝐝𝐬 Mr b² MD BOT`;
    var lien = mybotpic();
 
    if (lien.match(/\.(mp4|gif)$/i)) {
     try {
-        zk.sendMessage(dest, { video: { url: lien }, caption:infoMsg + menuMsg, footer: "Je suis *dullahmd*, déveloper dullah bot" , gifPlayback : true }, { quoted: ms });
+        zk.sendMessage(dest, { video: { url: lien }, caption:infoMsg + menuMsg, footer: "Je suis *Mr b²md*, déveloper Mr b² bot" , gifPlayback : true }, { quoted: ms });
     }
     catch (e) {
         console.log("🥵🥵 Menu erreur " + e);
@@ -70,7 +70,7 @@ let menuMsg = `
 // Vérification pour .jpeg ou .png
 else if (lien.match(/\.(jpeg|png|jpg)$/i)) {
     try {
-        zk.sendMessage(dest, { image: { url: lien }, caption:infoMsg + menuMsg, footer: "Je suis *dullahmd*, déveloper dullah bot" }, { quoted: ms });
+        zk.sendMessage(dest, { image: { url: lien }, caption:infoMsg + menuMsg, footer: "Je suis *Mr b²md*, déveloper Mr b² bot" }, { quoted: ms });
     }
     catch (e) {
         console.log("🥵🥵 Menu erreur " + e);
